21958ec10068b8210873fb09be78f2b59fae740b
KoofNG <isrealakpan296@gmail.com>
2019-02-19 12:52:46 +0100

    updates

--- lib/src/Home.dart
+++ lib/src/Home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import 'package:miscelleanous/src/Categories.dart';
 
 class Home extends StatelessWidget {
