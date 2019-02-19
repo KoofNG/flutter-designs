eb6221dc461e359efc4e73d40f866b11634ced56
KoofNG <israelakpan296@gmail.com>
2019-02-19 12:16:48 +0100

    Added the inteactivity for the progress bar to increase and decrease

--- lib/src/Categories.dart
+++ lib/src/Categories.dart
@@ -11,52 +11,53 @@ class _CategoriesState extends State<Categories> {
       'catImage': 'assets/icons/airtravel.png',
       'catName': 'Planes',
       'catCount': 103,
-      'selected':false
+      'selected': false
     },
     {
       'catImage': 'assets/icons/boat.png',
       'catName': 'Boat',
       'catCount': 73,
-      'selected':false
+      'selected': false
     },
     {
       'catImage': 'assets/icons/bus.png',
       'catName': 'Buses',
       'catCount': 457,
-      'selected':false
+      'selected': false
     },
     {
       'catImage': 'assets/icons/fastCar.png',
       'catName': 'Fast Cars',
       'catCount': 342,
-      'selected':false
+      'selected': false
     },
     {
       'catImage': 'assets/icons/run.png',
       'catName': 'Running',
       'catCount': 43,
-      'selected':false
+      'selected': false
     },
     {
       'catImage': 'assets/icons/scooter.png',
       'catName': 'Scooter',
       'catCount': 903,
-      'selected':false
+      'selected': false
     },
     {
       'catImage': 'assets/icons/ship.png',
       'catName': 'Ship',
       'catCount': 14,
-      'selected':false
+      'selected': false
     },
     {
       'catImage': 'assets/icons/yatch.png',
       'catName': 'Yatch',
       'catCount': 7,
-      'selected':false
+      'selected': false
     }
   ];
 
+  double _value = 0.0;
   @override
   Widget build(BuildContext context) {
     double _deviceWidth = MediaQuery.of(context).size.width;
@@ -89,7 +90,7 @@ class _CategoriesState extends State<Categories> {
           children: <Widget>[
             Container(
               width: _deviceWidth,
-              padding: EdgeInsets.symmetric(vertical: 15.0),
+              padding: EdgeInsets.symmetric(vertical: 10.0),
               child: Text(
                 'Which categories do you want to see the most?',
                 textAlign: TextAlign.left,
@@ -103,7 +104,7 @@ class _CategoriesState extends State<Categories> {
             ),
             Container(
               child: LinearProgressIndicator(
-                value: 0.1,
+                value: _value,
                 backgroundColor: Colors.grey.withOpacity(0.2),
               ),
             ),
@@ -121,20 +122,34 @@ class _CategoriesState extends State<Categories> {
                   ),
                   itemBuilder: (BuildContext context, int index) {
                     Map eachCategory = categories[index];
-                    return Container(
-                      height: _deviceHeight*0.25,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.pink[500], width: eachCategory['selected'] == true ? 2.5 : 0.0 // This is called the ternary operation, It's a shorter if-else statement,  look it up.)
+                    return Card(
+                      child: Container(
+                        height: _deviceHeight * 0.25,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4.0),
+                          border: Border.all(
+                              color: Colors.pink[500],
+                              width: eachCategory['selected'] == true
+                                  ? 2.5
+                                  : 0.0), // This is called the ternary operation, It's a shorter if-else statement,  look it up.)
+                        ),
+                        child: InkWell(
+                          onTap: () {
+                            setState(() {
+                              eachCategory['selected'] = !eachCategory['selected'];
+                            });
+                            if (eachCategory['selected'] == true){
+                              _value += 0.1;
+                            } else {
+                              _value -= 0.1;
+                            }
+                          },
+                          child: cardDisplay(
+                              eachCategory['catImage'],
+                              eachCategory['catName'],
+                              eachCategory['catCount']),
+                        ),
                       ),
-                      child: InkWell(
-                      onTap: () {
-                        setState(() {
-                          eachCategory['selected'] = !eachCategory['selected'];
-                        });
-                      },
-                      child: cardDisplay(eachCategory['catImage'],
-                          eachCategory['catName'], eachCategory['catCount']),
-                    )  
                     );
                   },
                 ),
@@ -172,53 +187,42 @@ class _CategoriesState extends State<Categories> {
   }
 
   Widget cardDisplay(String catImage, String catName, int catCount) {
-    return Container(
-      decoration: BoxDecoration(
-        /* border: Border.all(color: borderColor, width: 1.0, style: BorderStyle.solid), */
-        boxShadow: <BoxShadow>[
-          BoxShadow(
-              color: Colors.grey.withOpacity(0.2),
-              blurRadius: 10.0,
-              spreadRadius: 2.0)
-        ],
-      ),
-      child: Card(
-        margin: EdgeInsets.all(0.0),
-        elevation: 0.0,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                height: 60.0,
-                width: 70.0,
-                child: Image.asset(catImage),
-              ),
-              Container(
-                padding: EdgeInsets.all(2.0),
-                child: Text(
-                  catName,
-                  style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w500,
-                  ),
+    return Card(
+      margin: EdgeInsets.all(0.0),
+      elevation: 0.0,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 60.0,
+              width: 70.0,
+              child: Image.asset(catImage),
+            ),
+            Container(
+              padding: EdgeInsets.all(2.0),
+              child: Text(
+                catName,
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w500,
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(2.0),
-                child: Text(
-                  catCount.toString() + ' items',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w300,
-                  ),
+            ),
+            Container(
+              padding: EdgeInsets.all(2.0),
+              child: Text(
+                catCount.toString() + ' items',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w300,
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
